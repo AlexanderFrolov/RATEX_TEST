31b6474e904f1ce31dd99b0efa7a93c845ce32be
AlexanderFrolov <sovereignty.alex@gmail.com>
2022-08-12 19:55:40 +0300

    .

--- RatexTest/Task1/Server/Program.cs
+++ RatexTest/Task1/Server/Program.cs
@@ -1,156 +1,95 @@
-﻿using System.Net;
+﻿using ClientServerUdpFileExchangeWithTcpConfirmation;
 using System.Net.Sockets;
 using System.Text;
-
-//Server.exe 127.0.0.1 5555 temp
 
 if (args.Length < 3)
 {
     Console.WriteLine("Not enough parameters:");
-    Console.WriteLine(Helper.ServerHelperString());
+    Console.WriteLine(Server.Helper());
     Console.ReadLine();
+    return;
 }
-else
+
+Server s;
+
+try
 {
-    string address;             // адрес сервера (hostName или сам адресс)
-    int port;                   // порт сервера
-    string dirName;             // каталог для хранения файлов
+    s = new(args[0], Int32.Parse(args[1]), args[2]);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.GetType());
+    Console.WriteLine(Server.Helper());
+    return;
+}
 
-    int udpPort;                // переданный клиентом порт для приема файла по udp
-    string fileName;            // переданное клиентом имя файла
-    IPHostEntry ipHost;         // информация о хосте
-    IPAddress ipAddr;           // ip адресс хоста
-    IPEndPoint ipEndPoint;      // локальной точкой (ip + port), по которой будем принимать данные
-    Socket? tcpSocket = null;   // tcp cокет для прослушивания
-    Socket? udpSocket = null;   // udp сокет для передачт фала
-    int packetSize = 8192;      // определяем размер получаемых сегментов файла  
-    StringBuilder builder;  
-    
-    try
-    {
-        address = args[0];             
-        port = Int32.Parse(args[1]);    
-        dirName = args[2];              
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-        Console.WriteLine(ex.GetType());
-        Console.WriteLine(Helper.ServerHelperString());
-        return;
-    }
+try
+{
+    s.TcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    s.TcpSocket.Bind(s.IpEndPoint);
+    s.TcpSocket.Listen(10);
+    Console.Write("Server started. ");
 
-    try
+    while (true)
     {
-        ipHost = Dns.GetHostEntry(address);
-        ipAddr = ipHost.AddressList[0];
-        ipEndPoint = new IPEndPoint(ipAddr, port);
-        tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        tcpSocket.Bind(ipEndPoint);
-        tcpSocket.Listen(10);
-        Console.Write("Сервер запущен. ");
-
-        while (true)
+        try
         {
-            Console.WriteLine("Ожидание подключений...");
-            Socket? handler = null;
+            Console.WriteLine("Waiting for connections...");
 
-            try
+            s.Handler = null;
+            s.Handler = s.TcpSocket.Accept();        
+            string tcpResponse = s.ReceiveTcpMessage(); // tcp receive 1 (filename and udpPort)
+
+            if (tcpResponse == s.TCP_FAIL_STRING)
             {
-                handler = tcpSocket.Accept();
-                builder = receiveTcpMessage(handler, packetSize); // tcp receive 1 (имя файла и порт для udp)
-
-                if(builder.ToString() == "FAIL")
-                {
-                    Console.WriteLine("ошибка на стороне клиента. не найден файл или превышен размер файла");
-                    continue;
-                }
-                
-                Console.WriteLine("получено от client имя файла и udp порт: {0}", DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
-                string[] result = builder.ToString().Split(" ").ToArray();
-                fileName = result[0];
-                udpPort = Int32.Parse(result[1]);
-              
-                byte[] msg = Encoding.UTF8.GetBytes("OK");
-                handler.Send(msg); // tcp 1 send
-                                    
-                udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                IPEndPoint localIP = new IPEndPoint(ipAddr, udpPort);
-                udpSocket.Bind(localIP);
-
-                byte[] packetReceive = new byte[packetSize]; 
-                int bytes;
-                EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
-                bytes = udpSocket.ReceiveFrom(packetReceive, ref remoteIp); // udp receive 1  (количество сегментов)
-                int parts = BitConverter.ToInt32(packetReceive, 0);               
-                handler.Send(msg); // tcp 2 send
-                Directory.CreateDirectory(dirName);
-                string path = dirName + @"\" + fileName;
-
-                using (FileStream fStream = new FileStream(path, FileMode.Create, FileAccess.Write))
-                {                   
-                    for (int i = 0; i < parts; i++)
-                    {                       
-                        bytes = udpSocket.ReceiveFrom(packetReceive, ref remoteIp); // udp receive parts
-                        fStream.Write(packetReceive, 0, bytes);
-                        handler.Send(msg); // tcp 3 send                     
-                    }
-                }
-                
-                builder = receiveTcpMessage(handler, packetSize); // tcp receive 2
-                if(builder.ToString() == "OK")
-                {
-                    Console.WriteLine("клиент успешно получил все подтверждения. файл передан успешно!");
-                }
+                Console.WriteLine("Client side error. File not found or file size exceeded");
+                continue;
             }
-            catch(Exception ex)
+
+            string[] msg = tcpResponse.Split(" ");
+            string fileName = tcpResponse.Split(" ")[0];
+            int udpPort = Int32.Parse(tcpResponse.Split(" ")[1]);
+            Console.WriteLine("successfully received from client filename and udp port: {0}", tcpResponse);
+         
+            s.Handler.Send(s.TCP_OK_BYTE); // tcp 1 send
+            s.StartUdpReceiveFile(udpPort, fileName);
+
+            tcpResponse = s.ReceiveTcpMessage2(); // tcp receive 3
+            if (tcpResponse == s.TCP_OK_STRING)
             {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine(ex.GetType());             
+                Console.WriteLine("Client has successfully received all confirmations. File passed successfully!");
             }
-            finally
-            {
-                if (udpSocket != null)
-                {
-                    handler.Shutdown(SocketShutdown.Both);
-                    handler.Close();
-                }         
-                if (udpSocket != null)
-                {
-                    udpSocket.Shutdown(SocketShutdown.Both);
-                    udpSocket.Close();
-                    udpSocket = null;
-                }
-            }        
+
         }
-    }
-    catch(Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-        Console.WriteLine(ex.GetType());  
-    }
-    finally
-    {
-        if (tcpSocket != null)
+        catch (Exception ex)
         {
-            tcpSocket.Shutdown(SocketShutdown.Both);
-            tcpSocket.Close();
-            tcpSocket = null;
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.GetType());
+        }
+        finally
+        {
+            if(s.Handler != null)
+            {
+                s.Handler.Shutdown(SocketShutdown.Both);
+                s.Handler.Close();
+            }       
         }
     }
 }
-
-StringBuilder receiveTcpMessage(Socket handler, int packetSize)
+catch (Exception ex)
 {
-    StringBuilder builder = new StringBuilder();
-    int bytes = 0; 
-    byte[] data = new byte[256];
-    do
-    {
-        bytes = handler.Receive(data);
-        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
-    }
-    while (handler.Available > 0);
-
-    return builder;
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.GetType());
 }
+finally
+{
+    if (s.TcpSocket!= null)
+    {
+        s.TcpSocket.Shutdown(SocketShutdown.Both);
+        s.TcpSocket.Close();
+        s.TcpSocket = null;
+    }
+}
+
+
