b687dc7c319b6b2975efc11677f413ee890c1cf1
AlexanderFrolov <43673318+AlexanderFrolov@users.noreply.github.com>
2022-08-12 19:53:01 +0300

    Delete RatexTest/Task1/Server directory

--- RatexTest/Task1/Server/Helper.cs
+++
@@ -1,9 +0,0 @@
-﻿public static class Helper
-{
-    public static string ServerHelperString() =>
-
-        "Server <ip> <port> <dirName> \n" +
-        "\t ip - ip address of the server (hostName or address) \n " +
-        "\t port - port to connect to the server \n" +
-        "\t dirName - directory for storing files";
-}
